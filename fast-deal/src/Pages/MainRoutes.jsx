f61ceb9cb00741dcf67936c105b89f3f9ea979ce
Koushik niyogi <koushikniyogionlinework@gmail.com>
2023-03-29 12:54:17 +0530

    done

--- fast-deal/src/Pages/MainRoutes.jsx
+++ fast-deal/src/Pages/MainRoutes.jsx
@@ -26,23 +26,6 @@ const MainRoutes = () => {
             <h1>Error 404</h1>
             <h2>Page Not Foud</h2>
       </div>}/>
-=======
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Cart from "../Components/Cart";
-import HomeandAppliance from "./HomeandAppliance";
-import HomePage from "./HomePage";
-import MensPage from "./MensPage";
-import WomensPage from "./WomensPage";
-const MainRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/menspage" element={<MensPage />} />
-      <Route path="/womenpage" element={<WomensPage />} />
-      <Route path="/homeandappliances" element={<HomeandAppliance />} />
-      <Route path="/cart" element={<Cart />} />
->>>>>>> a261fb2c29a2b68093bf299a3e2430cfc8fca879
     </Routes>
   );
 };
