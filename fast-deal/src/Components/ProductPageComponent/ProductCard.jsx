e24006c8aeb341a33989a568abbab29e68a87282
Rajesh Pradhan <pradhan143rajesh@gmail.com>
2023-04-02 00:24:56 +0530

    day--4

--- fast-deal/src/Components/ProductPageComponent/ProductCard.jsx
+++ fast-deal/src/Components/ProductPageComponent/ProductCard.jsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import { Box, Flex, Image, Text } from "@chakra-ui/react"
-import { SlHeart } from "react-icons/sl"
-import Stars from './Stars';
-import { Skeleton, Stack } from '@chakra-ui/react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { SlHeart } from "react-icons/sl";
+import Stars from "./Stars";
+import { Skeleton, Stack } from "@chakra-ui/react";
 const ProductCard = ({ item }) => {
   return (
     <Link to={`/product/${item.id}`}>
-    <Box padding={"10px"} textAlign={"start"} _hover={{
-      boxShadow: "0 5px 17px 0 rgba(23,84,116,0.18)",
-      zIndex: "15"
-    }}>
-      <Flex justifyContent={"flex-end"}>
-        <SlHeart />
-      </Flex>
-      <Image
-        boxSize='250px'
-        objectFit='cover'
-        src={item.image[0]}
-        alt='Dan Abramov'
-
-      />
-      <Text fontSize='xl' color={"#666"}>{item.name}</Text>
-      <Flex alignItems="center">
-        <Text textDecoration={"line-through"} fontSize='md' color={"#666"} mr={"10px"}>Rs. {item["strikeoff-price"]}</Text>
-        <Text fontSize='lg' color={"black"} mr={"10px"}>Rs. {item.price}</Text>
-        <Text fontSize='md' color={"#666"} border={"1px solid #666"} padding={"2px"}>{item.discount}% off</Text>
-      </Flex>
-      <Flex  alignItems="center" ><Stars rating = {item.rating.rate} count ={item.rating.count}/><span style={{marginLeft : "15px"}}>({item.rating.count} votes)</span></Flex>
-      <Text fontSize='md' color={"#666"}>Category : {item.brand || item.category}</Text>
-    </Box>
+      <Box
+        padding={"10px"}
+        textAlign={"start"}
+        _hover={{
+          boxShadow: "0 5px 17px 0 rgba(23,84,116,0.18)",
+          zIndex: "15",
+        }}
+      >
+        <Flex justifyContent={"flex-end"}>
+          <SlHeart />
+        </Flex>
+        <Image
+          boxSize="100%"
+          objectFit="cover"
+          src={item.image[0]}
+          alt="Dan Abramov"
+          margin={"auto"}
+        />
+        <Text fontSize="l" color={"#666"}>
+          {item.name}
+        </Text>
+        <Flex alignItems="center">
+          <Text
+            textDecoration={"line-through"}
+            fontSize="md"
+            color={"#666"}
+            mr={"10px"}
+          >
+            Rs. {item["strikeoff-price"]}
+          </Text>
+          <Text fontSize="lg" color={"black"} mr={"10px"}>
+            Rs. {item.price}
+          </Text>
+          <Text
+            fontSize="md"
+            color={"white"}
+            border={"1px solid #666"}
+            padding={"2px"}
+            bgColor={"green"}
+          >
+            {item.discount}% off
+          </Text>
+        </Flex>
+        <Flex alignItems="center">
+          <Stars rating={item.rating.rate} count={item.rating.count} />
+          <span style={{ marginLeft: "15px" }}>
+            ({item.rating.count} votes)
+          </span>
+        </Flex>
+        <Text fontSize="md" color={"#666"}>
+          Category : {item.brand || item.category}
+        </Text>
+      </Box>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- fast-deal/src/Homepage/HomeMid.js
+++ fast-deal/src/Homepage/HomeMid.js
@@ -6,31 +6,33 @@ import { Banner } from "./Banner";
 
 import { Pincode } from "./PinCode";
 import { SideProductList } from "./SideProductList";
+import ProductSlider from "../Components/Carosal/CardCarosal";
 
-export class HomeMid extends Component {
-  render() {
-    return (
-      <div className="main-bar">
-        <div className="main-bar__container">
-          <div className="main-bar__sidebar">
-            <SideProductList />
+function HomeMid() {
+  return (
+    <div className="main-bar">
+      <div className="main-bar__container">
+        <div className="main-bar__sidebar">
+          <SideProductList />
+        </div>
+        <div className="main-bar__carousel">
+          <div className="main-bar__pin-cont">
+            <div className="main-bar__banner-carousel">
+              <Banner />
+            </div>
+            <div className="main-bar__pin">
+              <Pincode />
+            </div>
           </div>
-          <div className="main-bar__carousel">
-            <div className="main-bar__pin-cont">
-              <div className="main-bar__banner-carousel">
-                <Banner />
-              </div>
-              <div className="main-bar__pin">
-                <Pincode />
-              </div>
-            </div>
-            <h2 className="main-bar__trending">TRENDING PRODUCTS</h2>
-            <div className="main-bar__products-carousel">
-              {/* ALL Cards willbe herer */}
-            </div>
+          <h2 className="main-bar__trending">TRENDING PRODUCTS</h2>
+          <div className="main-bar__products-carousel">
+            {/* ALL Cards willbe herer */}
+            <ProductSlider />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export { HomeMid };
